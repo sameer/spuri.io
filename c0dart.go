828b4d7d301f87aec6483582fdce74719f178936
Sameer <spuri4096@gmail.com>
2018-01-04 14:54:49 -0500

    Add image output for c0dart

--- c0dart.go
+++ c0dart.go
@@ -4,23 +4,31 @@ import (
 	"net/http"
 	"io/ioutil"
 	"fmt"
+	"github.com/tobi/mogrify-go"
 )
 
 type C0dartContext struct {
 	*GlobalContext
-	Images []string
+	Images []C0dartImage
 }
 
+type C0dartImage struct {
+	Name string
+	Href string
+}
+
+var igs []mogrify.Image = nil
+
 func c0dartHandler(w http.ResponseWriter, r *http.Request) {
-	var names []string = nil
+	var c0dartImages []C0dartImage= nil
 	if images, err := ioutil.ReadDir("static/c0dart"); err == nil {
-		names = make([]string, len(images))
+		c0dartImages = make([]C0dartImage, len(images))
 		for i, img := range images {
-			names[i] = img.Name()
+			c0dartImages[i] = C0dartImage{Name: img.Name(), Href: "/static/c0dart/" + img.Name()}
 		}
 	} else {
 		fmt.Printf("Error reading c0dart directory: %v\n", err)
 	}
-	ctx := C0dartContext{ GlobalContext: globalContext, Images: names}
+	ctx := C0dartContext{ GlobalContext: globalContext, Images: c0dartImages}
 	renderTemplate(w, "c0dart", ctx)
 }
